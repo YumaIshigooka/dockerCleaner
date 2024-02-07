15f75c0c03b1073fb6160ce054d29d3382cb4f5e
YumaIshigooka <yuma.ishigooka01@estudiant.upf.edu>
2024-02-07 11:29:43 +0100

    comments

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # dockerCleaner
-Clean stopped containers &amp; their respective images all at the same time. 
+Clean all "<none>" images, and their respective stopped container that was running it, not allowing to delete the images.
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/YumaIshigooka/dockerCleaner.git
+```
+
+Go to the project directory
+
+```bash
+  cd dockerCleaner
+```
+
+Run the script (will ask for permissions, check the source code to ensure the reason)
+
+```bash
+  python3 cleanDocker.py
+```
+
+If you know how to remove the need of admin privileges, let me know.
+
--- cleanDocker.py
+++ cleanDocker.py
@@ -2,14 +2,21 @@ import os
 import re
 
 if __name__ == "__main__":
+    # List all the Images and Exited Containers.
     os.system("sudo docker image ls > img_list.txt")
     os.system("sudo docker ps --filter \"status=exited\" > container_list.txt")
     f_images = open("img_list.txt", "r")
     f_cont = open("container_list.txt", "r")
+
+    # The final commands
     cont_cmd = "sudo docker rm "
     img_cmd = "sudo docker rmi "
+
+    # To only remove containers listed on the image list.
     img_set = set()
     idx = 0
+
+    # Archive the images into the set, and build the rmi command
     lines = f_images.readlines()
     for l in lines:
         if idx != 0:
@@ -19,6 +26,9 @@ if __name__ == "__main__":
                 img_cmd += split[2] + " "
         idx += 1
     idx = 0
+
+    # Build the rm command for the containers. 
+    # If the image referenced by the container is listed in the to delete set, delete the container.
     lines = f_cont.readlines()
     for l in lines:
         if idx != 0:
@@ -26,11 +36,11 @@ if __name__ == "__main__":
             if split[1] in img_set:
                 cont_cmd += split[0] + " "
         idx += 1
+    
+    # Run the containers.
     os.system(cont_cmd)
     os.system(img_cmd)
 
-    
-    
-    
+    # Remove the files.    
     os.system("rm *.txt")
     
